969e47db12aded79e6c7996dfa3da7b96f9280b0
JanGodar <2017sofin@gmail.com>
2024-09-12 10:50:08 +0300

    handler

--- tg_bot_1/handlers/user_handlers.py
+++ tg_bot_1/handlers/user_handlers.py
@@ -41,7 +41,7 @@ async def process_start_command(message, state):
 
     await state.update_data(attempt = attempt + 1)
 
-    await state.clear()
+    #await state.clear()
     val = await state.get_state()
     logger.info('%s', val)
 
